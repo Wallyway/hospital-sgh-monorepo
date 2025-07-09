fc8869f8c2dc8c373aa025964494c17052f23991
Sebastian Wallis <j.sebastian.aw@gmail.com>
2025-07-09 02:02:58 -0500

    fix: correct import path for EquipmentService in equipment.controller.ts

--- apps/equipment-service/src/equipment/equipment.controller.ts
+++ apps/equipment-service/src/equipment/equipment.controller.ts
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { Controller, Get, Post, Body, Req } from '@nestjs/common';
-import { EquipmentService } from './ equipment.service';
+import { EquipmentService } from './equipment.service';
 import { RequestEquipmentDto } from '../dto/request-equipment.dto';
 import { EquipmentResponseDto } from '../dto/equipment-response.dto';
 
